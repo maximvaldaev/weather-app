bd3c4249a046bbeaa7d0944adc91a090d52ad909
Maxim Valdaev <valdaev.m@yandex.ru>
2024-03-13 22:16:32 +0300

    Добавлена работа со статусами tab в bav bar

--- src/app/right-container/right-container.component.ts
+++ src/app/right-container/right-container.component.ts
@@ -8,5 +8,40 @@ import { Component } from '@angular/core';
   styleUrl: './right-container.component.css'
 })
 export class RightContainerComponent {
+  // var to controle tabs
+  today:boolean = false;
+  week:boolean = true;
+
+  // var to controle metric values
+  celcius:boolean = true;
+  fahrenheit:boolean = false;
+
+  //function to controle tab values or tabs status
+  
+  //function for click on tab Today
+  onTodayClick(){
+    this.today = true;
+    this.week = false;
+  }
+  //function for click on tab Week
+  onWeekClick(){
+    this.week = true;
+    this.today = false;
+  }
+
+  //function to controle martic values
+
+  //function for click on tab Celcius
+  onCelciusClick(){
+    this.celcius = true;
+    this.fahrenheit = false;
+  }
+
+  //function for click on tab Fahrenheit
+  onFahrenheitClick(){
+    this.fahrenheit = true;
+    this.celcius = false;
+  }
+
 
 }
