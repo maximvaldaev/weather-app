d6ec64baed25f488af5038cc94622b8aa33a5073
Maxim Valdaev <valdaev.m@yandex.ru>
2024-03-13 22:49:27 +0300

    Добавлены инпут и иконки поиска и местоположения

--- src/app/left-container/left-container.component.ts
+++ src/app/left-container/left-container.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faMagnifyingGlass, faLocation } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-left-container',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './left-container.component.html',
   styleUrl: './left-container.component.css'
 })
 export class LeftContainerComponent {
+  //var for font awesome icons
 
+  //var for ;eft-nav-bar seach icon
+  faMagnifyingGlass:any = faMagnifyingGlass;
+  faLocation:any = faLocation;
 }
